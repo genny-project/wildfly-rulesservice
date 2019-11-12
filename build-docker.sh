1824e98f02af8e8c088c16298cd4cd8eab572b2d
Jack Li <hbjoylee@gmail.com>
2019-11-12 12:19:56 +1100

    Update build and push docker images scripts

--- build-docker.sh
+++ build-docker.sh
@@ -3,30 +3,30 @@
 project=wildfly-rulesservice
 file="rulesservice-war/src/main/resources/rulesservice-war-git.properties"
 
-function prop {
-    grep "${1}=" ${file}|cut -d'=' -f2
+function prop() {
+  grep "${1}=" ${file} | cut -d'=' -f2
 }
 
 if [ -z "${1}" ]; then
-   version="latest"
+  version="latest"
 else
-   version="${1}"
+  version="${1}"
 fi
 
-
-if [ -f "$file" ]
-then
+if [ -f "$file" ]; then
   echo "$file found."
+  echo "git.commit.id = " "$(prop 'git.commit.id')"
+  echo "git.build.version = " "$(prop 'git.build.version')"
 
-  echo "git.commit.id = " $(prop 'git.commit.id')
-  echo "git.build.version = " $(prop 'git.build.version')
-  docker build  -t gennyproject/${project}:${version} --network host .
-  docker tag gennyproject/${project}:${version} gennyproject/${project}:$(prop 'git.commit.id')
-  docker tag gennyproject/${project}:${version} gennyproject/${project}:$(prop 'git.build.version')
+  docker build -t gennyproject/${project}:"${version}" --network host .
 else
   echo "ERROR: git properties $file not found."
 fi
 
-#create the dummy container that is used in docker-compose to disable things easier
-docker pull gennyproject/dummy:latest
-docker tag gennyproject/dummy:latest gennyproject/${project}:dummy
+#clean up
+image_ids=$(docker images | grep ${project} | grep none)
+if [ "${image_ids:-0}" == 0 ]; then
+  echo 'Skip clean up'
+else
+  docker images | grep ${project} | grep none | awk '{print $3}' | xargs docker rmi
+fi
--- push.sh
+++ push.sh
@@ -2,27 +2,33 @@
 project=wildfly-rulesservice
 file="rulesservice-war/src/main/resources/rulesservice-war-git.properties"
 
-function prop {
-    grep "${1}=" ${file}|cut -d'=' -f2
+function prop() {
+  grep "${1}=" ${file} | cut -d'=' -f2
 }
 
 if [ -z "${1}" ]; then
-   version="latest"
+  version="latest"
 else
-   version="${1}"
+  version="${1}"
 fi
 
-if [ -f "$file" ]
-then
+if [ -f "$file" ]; then
   echo "$file found."
+  echo "git.commit.id = " "$(prop 'git.commit.id')"
+  echo "git.build.version = " "$(prop 'git.build.version')"
 
-  echo "git.commit.id = " $(prop 'git.commit.id')
-  echo "git.build.version = " $(prop 'git.build.version')
-  docker push gennyproject/wildfly-rulesservice:latest
   docker push gennyproject/wildfly-rulesservice:"${version}"
-  docker push gennyproject/${project}:$(prop 'git.commit.id')
-  docker push gennyproject/${project}:$(prop 'git.build.version')
+
+  docker tag gennyproject/${project}:"${version}" gennyproject/${project}:latest
+  docker push gennyproject/wildfly-rulesservice:latest
+
+  docker tag gennyproject/${project}:"${version}" gennyproject/${project}:"$(prop 'git.build.version')"
+  docker push gennyproject/${project}:"$(prop 'git.build.version')"
+
+  #create the dummy container that is used in docker-compose to disable things easier
+  docker pull gennyproject/dummy:latest
+  docker tag gennyproject/dummy:latest gennyproject/${project}:dummy
+  docker push gennyproject/${project}:dummy
 else
   echo "ERROR: git properties $file not found."
 fi
-
