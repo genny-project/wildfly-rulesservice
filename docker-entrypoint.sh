67fa9aba93e34e45d2021b5606439ac307103d5f
Adam Crow <adamcrow63@gmail.com>
2020-01-17 00:14:44 +1100

    Updated Xms and Xmx

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,19 +16,20 @@ fi
 if [ $XMX ] ; then
   echo "JVM XMX = "$XMX
   sed -i 's,-Xmx512m,-Xmx'"$XMX"',g' /opt/jboss/wildfly/bin/standalone.conf
+  sed -i 's,-Xms64m,-Xms'"$XMX"',g' /opt/jboss/wildfly/bin/standalone.conf
 fi
 
     /opt/jboss/wildfly/bin/add-user.sh  jmsuser jmspassword1
 
 if [[ $DEBUG == "TRUE" ]]; then
    echo "Remote Debug on port 8787 True";
-   export JAVA_OPTS="${JAVA_OPTS} -Xms2048m -Xmx2048m  -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=${DEBUG_SUSPEND:=n}  -Drebel.remoting_plugin=true "
+   export JAVA_OPTS="${JAVA_OPTS}  -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=${DEBUG_SUSPEND:=n}  -Drebel.remoting_plugin=true "
    /opt/jboss/wildfly/bin/standalone.sh --debug -Dorg.kie.task.insecure=true  -Djboss.bind.address.private=$myip  -bmanagement=0.0.0.0 -b 0.0.0.0 -Dadmin.username=${ADMIN_USERNAME} -Dadmin.password=${ADMIN_PASSWORD} -Dpublic.host=${myip}  -DHIBERNATE_SHOW_SQL=${HIBERNATE_SHOW_SQL:=false} -DHIBERNATE_HBM2DDL=$HIBERNATE_HBM2DDL -DMYSQL_USER=$MYSQL_USER -DMYSQL_PASSWORD=$MYSQL_PASSWORD -Djava.security.auth.login.config=''  -b 0.0.0.0
 #--server-config=standalone-full-ha.xml
 else
    export JAVA_OPTS="${JAVA_OPTS} "
    #export JAVA_OPTS="${JAVA_OPTS} -agentpath:/usr/local/YourKit-JavaProfiler-2019.8/bin/linux-x86-64/libyjpagent.so=port=10002,listen=all"
    echo "Debug is False";
-   /opt/jboss/wildfly/bin/standalone.sh -Xms2048m -Xmx2048m -Djboss.bind.address.private=$myip -Dorg.kie.task.insecure=true  -bmanagement=0.0.0.0 -b 0.0.0.0 -Dadmin.username=${ADMIN_USERNAME} -Dadmin.password=${ADMIN_PASSWORD} -Dpublic.host=${myip}
+   /opt/jboss/wildfly/bin/standalone.sh -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -Djboss.bind.address.private=$myip -Dorg.kie.task.insecure=true  -bmanagement=0.0.0.0 -b 0.0.0.0 -Dadmin.username=${ADMIN_USERNAME} -Dadmin.password=${ADMIN_PASSWORD} -Dpublic.host=${myip}
  #  -DHIBERNATE_SHOW_SQL=$HIBERNATE_SHOW_SQL -DHIBERNATE_HBM2DDL=$HIBERNATE_HBM2DDL -DMYSQL_USER=$MYSQL_USER -DMYSQL_PASSWORD=$MYSQL_PASSWORD -Djava.security.auth.login.config=''
 fi
