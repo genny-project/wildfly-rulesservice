bfc1cb5a0b0807bf255c43f99d2bafb485f72879
Jian Li <hbjoylee@gmail.com>
2020-04-29 11:55:22 +1000

    Cunt unfired rules

--- rulesservice-war/src/main/java/life/genny/qwanda/service/StartupService.java
+++ rulesservice-war/src/main/java/life/genny/qwanda/service/StartupService.java
@@ -1,6 +1,7 @@
 package life.genny.qwanda.service;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -10,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.util.concurrent.TimeUnit;
+
 import org.apache.logging.log4j.Logger;
 import org.jboss.ejb3.annotation.TransactionTimeout;
 
@@ -30,50 +32,58 @@ import life.genny.utils.FrameUtils2;
 @Singleton
 @Startup
 @Transactional
-@TransactionTimeout(value=4500, unit=TimeUnit.SECONDS)
+@TransactionTimeout(value = 4500, unit = TimeUnit.SECONDS)
 public class StartupService {
 
-	/**
-	 * Stores logger object.
-	 */
-	protected static final Logger log = org.apache.logging.log4j.LogManager
-			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+    /**
+     * Stores logger object.
+     */
+    protected static final Logger log = org.apache.logging.log4j.LogManager
+            .getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 
-	@Inject
-	private SecurityService securityService;
-	
-	@Inject
-	private SecureResources secureResources;
+    @Inject
+    private SecurityService securityService;
 
-	@Inject
-	private RulesService rulesservice;
-	
-	@PostConstruct
-	@Transactional
-	@TransactionTimeout(value=4500, unit=TimeUnit.SECONDS)
-	public void init() {
-		log.info("---------------- Commencing Startup - v 3.1.0  ----------------");
-		long startTime = System.nanoTime();
+    @Inject
+    private SecureResources secureResources;
 
-		rulesservice.init();
-		securityService.setImportMode(false); // force this to start up
-		secureResources.setup(); // force start up
-		
-		// Now check if themes and frames loaded up
-		log.info("Checking "+FrameUtils2.ruleFires.keySet().size()+" rules for unfired rules ...");
-		
-		for (String rule : FrameUtils2.ruleFires.keySet()) {
-			if ( !FrameUtils2.ruleFires.get(rule)) {
-				log.error(rule+" DID NOT FIRE!!");
-			}
-		}
-		log.info("Rule checks done. ");
+    @Inject
+    private RulesService rulesservice;
 
-		double difference = ( System.nanoTime() - startTime) / 1e9; // get s
+    @PostConstruct
+    @Transactional
+    @TransactionTimeout(value = 4500, unit = TimeUnit.SECONDS)
+    public void init() {
+        log.info("---------------- Commencing Startup - v 3.1.0  ----------------");
+        long startTime = System.nanoTime();
 
-		log.info("---------------- Completed Startup in "+difference+" sec ----------------");
-	}
+        rulesservice.init();
+        securityService.setImportMode(false); // force this to start up
+        secureResources.setup(); // force start up
 
+        // Now check if themes and frames loaded up
+        log.info("Checking " + FrameUtils2.ruleFires.keySet().size() + " rules for unfired rules ...");
+
+        List<String> fullRuleList = new ArrayList<>();
+        List<String> notFiredRuleList = new ArrayList<>();
+
+        for (String rule : FrameUtils2.ruleFires.keySet()) {
+            fullRuleList.add(rule);
+            if (!FrameUtils2.ruleFires.get(rule)) {
+                notFiredRuleList.add(rule);
+            }
+        }
+        log.info("Rule checks done. ");
+        log.info("Elements in FullRuleList: " + fullRuleList.size());
+        log.info("Elements in NotFiredRuleList: " + notFiredRuleList.size());
+        log.info("FullRuleList: " + fullRuleList);
+        log.info("NotFiredRuleList: " + notFiredRuleList);
+
+
+        double difference = (System.nanoTime() - startTime) / 1e9; // get s
+
+        log.info("---------------- Completed Startup in " + difference + " sec ----------------");
+    }
 
 
 }
