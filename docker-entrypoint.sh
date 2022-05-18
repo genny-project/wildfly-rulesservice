18fdb69564696fbae96069859983613045ae1a95
Shannon <shannon@gada.io>
2022-05-18 10:31:38 +0000

    Changing the jboss.bind.address.private to 127.0.0.1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,12 +24,12 @@ fi
 if [[ $DEBUG == "TRUE" ]]; then
    echo "Remote Debug on port 8787 True";
    export JAVA_OPTS="${JAVA_OPTS}   -agentlib:jdwp=transport=dt_socket,address=*:8787,server=y,suspend=${DEBUG_SUSPEND:=n}  -Drebel.remoting_plugin=true "
-   /opt/jboss/wildfly/bin/standalone.sh --debug -Dorg.kie.task.insecure=true  -Djboss.bind.address.private=$myip  -bmanagement=0.0.0.0 -b 0.0.0.0 -Dadmin.username=${ADMIN_USERNAME} -Dadmin.password=${ADMIN_PASSWORD} -Dpublic.host=${myip}  -DHIBERNATE_SHOW_SQL=${HIBERNATE_SHOW_SQL:=false} -DHIBERNATE_HBM2DDL=$HIBERNATE_HBM2DDL -DMYSQL_USER=$MYSQL_USER -DMYSQL_PASSWORD=$MYSQL_PASSWORD -Djava.security.auth.login.config=''  -b 0.0.0.0
+   /opt/jboss/wildfly/bin/standalone.sh --debug -Dorg.kie.task.insecure=true  -Djboss.bind.address.private=127.0.0.1  -bmanagement=0.0.0.0 -b 0.0.0.0 -Dadmin.username=${ADMIN_USERNAME} -Dadmin.password=${ADMIN_PASSWORD} -Dpublic.host=${myip}  -DHIBERNATE_SHOW_SQL=${HIBERNATE_SHOW_SQL:=false} -DHIBERNATE_HBM2DDL=$HIBERNATE_HBM2DDL -DMYSQL_USER=$MYSQL_USER -DMYSQL_PASSWORD=$MYSQL_PASSWORD -Djava.security.auth.login.config=''  -b 0.0.0.0
 #--server-config=standalone-full-ha.xml
 else
    export JAVA_OPTS="${JAVA_OPTS} "
    #export JAVA_OPTS="${JAVA_OPTS} -agentpath:/usr/local/YourKit-JavaProfiler-2019.8/bin/linux-x86-64/libyjpagent.so=port=10002,listen=all"
    echo "Debug is False";
-   /opt/jboss/wildfly/bin/standalone.sh -Dcom.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize=true -Djboss.bind.address.private=$myip -Dorg.kie.task.insecure=true  -bmanagement=0.0.0.0 -b 0.0.0.0 -Dadmin.username=${ADMIN_USERNAME} -Dadmin.password=${ADMIN_PASSWORD} -Dpublic.host=${myip}
+   /opt/jboss/wildfly/bin/standalone.sh -Dcom.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize=true -Djboss.bind.address.private=127.0.0.1 -Dorg.kie.task.insecure=true  -bmanagement=0.0.0.0 -b 0.0.0.0 -Dadmin.username=${ADMIN_USERNAME} -Dadmin.password=${ADMIN_PASSWORD} -Dpublic.host=${myip}
  #  -DHIBERNATE_SHOW_SQL=$HIBERNATE_SHOW_SQL -DHIBERNATE_HBM2DDL=$HIBERNATE_HBM2DDL -DMYSQL_USER=$MYSQL_USER -DMYSQL_PASSWORD=$MYSQL_PASSWORD -Djava.security.auth.login.config=''
 fi
