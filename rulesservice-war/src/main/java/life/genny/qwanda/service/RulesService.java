01f4ad2d243d88179cfdc71c018d05b6e8ed3643
varunshastry <varunshastry@rediffmail.com>
2022-05-26 16:37:44 +0530

    Changing to json object from string

--- rulesservice-war/src/main/java/life/genny/qwanda/service/RulesService.java
+++ rulesservice-war/src/main/java/life/genny/qwanda/service/RulesService.java
@@ -56,7 +56,7 @@ public class RulesService {
 		JsonObject skipJson = VertxUtils.readCachedJson("JENNY", "SKIP");
 		if (skipJson.containsKey("status")) {
 			if ("ok".equalsIgnoreCase(skipJson.getString("status"))) {
-				String val = skipJson.getString("value");
+				String val = skipJson.getJsonObject("value").toString();
 				if ("TRUE".equalsIgnoreCase(val)) {
 					noChangeInRules = true;
 					noskip = false;
